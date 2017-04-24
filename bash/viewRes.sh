d01a26257b797ef64149b4e3cef1acdbe3fabb7e
Christopher Steele <steelecd2110@hotmail.com>
2017-04-24 10:55:26 +0100

    Added viewRes.sh

---
+++ bash/viewRes.sh
@@ -0,0 +1,12 @@
+phyloDir=$1
+resDir=$2
+samples=${ls resDir}
+for i in $samples; do
+	source jsonRes.sh $resDir $i $phyloDir
+	cp -r $resDir/$i/$i $phyloDir/witness/data/
+	gunzip $phyloDir/witness/data/*/*.gz
+done
+python2 $phyloDir/witness/index_data.py
+python2 -m $phyloDir/witness/SimpleHTTPServer
+
+
