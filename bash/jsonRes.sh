c64716e335db26a4772657a6c8a907361f039dcd
Christopher Steele <steelecd2110@hotmail.com>
2017-04-24 10:44:30 +0100

    altered json creation

--- bash/jsonRes.sh
+++ bash/jsonRes.sh
@@ -2,6 +2,6 @@
 resDir=$1
 sample=$2
 phyloDir=$3
-mkdir $resDir/test_results
-cd $resDir/test_results
-python2 $phyloDir/write_results.py $sample $resDir/trees.zip $resDir/test_results/$sample".summ.json.gz" $resDir/test_results/$sample".muts.json.gz" $resDir/test_results/$sample".mutass.zip"
+mkdir $resDir/$sample
+cd $resDir/$sample
+python2 $phyloDir/write_results.py $sample $resDir/trees.zip $resDir/$sample/$sample".summ.json.gz" $resDir/$sample/$sample".muts.json.gz" $resDir/$sample/$sample".mutass.zip"
