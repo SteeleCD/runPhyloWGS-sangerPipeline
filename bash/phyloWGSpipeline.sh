724fd9a7154287eb27deeec1d1afd96b3de47204
Christopher Steele <steelecd2110@hotmail.com>
2017-04-26 22:20:42 +0100

    Fixed folder changing issue

--- bash/phyloWGSpipeline.sh
+++ bash/phyloWGSpipeline.sh
@@ -7,10 +7,6 @@ bulkVcf=/nfs/users/nfs_c/cs32/results/bulkConvert/all_variants_30012017.txt
 purityFile=/nfs/users/nfs_c/cs32/data/ploidyPurityWGS.csv
 sample=$1
 
-
-mkdir $outDir/$sample
-cd $outDir/$sample
-
 # convert sanger vcf to mutect vcf
 /software/R-3.3.2/bin/Rscript --vanilla ../R/sangerToMutectPhylo.R "$sample"a $bulkVcf $vcfDir
 
@@ -21,6 +17,11 @@ cat ../misc/vcfHeads.txt $vcfDir/"$sample"a.vcf > $vcfDir/"$sample"a.vcf
 contam=$(grep $sample $purityFile | awk -F "," '{print $2}')
 contam=$(echo "1 - $contam"|bc)
 
+# change folder
+currDir=$(pwd)
+mkdir $outDir/$sample
+cd $outDir/$sample
+
 # generate cnv intermediate files
 python $phyloDir/parser/parse_cnvs.py -f battenberg -c $contam $bbDir/$sample/tmpBattenberg/"$sample"a_subclones.txt
 
@@ -29,3 +30,5 @@ python $phyloDir/parser/create_phylowgs_inputs.py --cnvs sample1=$outDir/$sample
 
 # run phyloWGS
 bsub -q long -R"select[mem>45000] rusage[mem=45000]" -M45000 -J "phylo-$sample" -o $lustreDir/logs/out/phylo-$sample.%J.out -e $lustreDir/logs/error/phylo-$sample.%J.out "cd $outDir/$sample && python $phyloDir/evolve.py --params $outDir/$sample/params.json $outDir/$sample/ssm_data.txt $outDir/$sample/cnv_data.txt"
+
+cd $currDir
